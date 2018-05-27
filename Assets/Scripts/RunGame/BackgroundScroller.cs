16d21d85d28e40c0fbab5273f4dbc9f4c91b6536
Kyuho Lee <modulo641@gmail.com>
2018-05-27 17:28:51 +0900

    유니티 스터디 파일 업로드

---
+++ Assets/Scripts/RunGame/BackgroundScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunGame
+{
+	public class BackgroundScroller : MonoBehaviour 
+	{
+		MeshRenderer renderer;
+
+		float xOffset;
+
+		public float scrollingSpeed;
+
+		// Use this for initialization
+		void Start () 
+		{
+			xOffset = 0;
+			renderer = GetComponent<MeshRenderer>();
+		}
+
+		// Update is called once per frame
+		void Update () 
+		{
+			xOffset += Time.deltaTime * scrollingSpeed;
+			Vector2 offset = new Vector2(xOffset, 0);
+			renderer.material.SetTextureOffset("_MainTex", offset);
+		}
+	}
+}
---
+++ Assets/Scripts/RunGame/CharacterManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunGame
+{
+	public class CharacterManager : MonoBehaviour 
+	{
+		bool isGround = false;
+
+		float ySpeed;
+
+		public float jumpPower = 5;
+
+		public float gravity = -9.8f;
+
+		void Update()
+		{
+			if (!isGround)
+			{
+				ySpeed += gravity * Time.deltaTime;
+				transform.position += new Vector3(0, ySpeed * Time.deltaTime, 0);
+			}
+
+			if (isGround & Input.GetKeyDown(KeyCode.Space))
+			{
+				Jump();
+			}
+		}
+
+		void Jump()
+		{
+			isGround = false;
+			ySpeed = jumpPower;
+		}
+
+		void OnTriggerEnter(Collider coll)
+		{
+			isGround = true;
+			ySpeed = 0;
+		}
+	}
+}
